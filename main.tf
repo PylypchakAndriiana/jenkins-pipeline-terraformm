6becb4bbd3cf925329522698f9d7845f7460108c
PylypchakAndriiana <116636541+PylypchakAndriiana@users.noreply.github.com>
2026-04-09 10:58:33 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.network_name
+  auto_create_subnetworks = true
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "${var.network_name}-allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "vm" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc.id
+
+    access_config {
+    }
+  }
+
+  tags = ["ssh"]
+}
